44cd520afbad9ac9b4d6c04187225e1a29cdbe5e
Denis <std.approach@gmail.com>
2019-05-23 10:38:49 +0200

    First commit for Equal Sytems

---
+++ eqSystem/example1.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sys
+sys.path.append('../ODE_Delta/raw')
+
+import helper as hlp
+
+T1 = .1
+x10 = T1
+
+title1 = 'Example1. Oliveira p.3'
+
+def y_ex1(t):
+	return T1*(np.exp(-t*T1))
+
+va1 = [1, T1]
+IC10 = [x10]
+t0 = 0
+tb = 10
+N = 100
+#
+T2 = 0.5
+x20 = T2
+va2 = [1, T2]
+IC20 = [x20]
+
+
+Z1 = hlp.mkSlnT0(va1, IC10, t0, tb, N)
+Y1 = hlp.mkSlnT0(va2, IC20, t0, tb, N)
+
+C2 = x10/x20
+Y2 = Y1
+Y2[:,1] = C2*Y1[:,1]
+
+plt.xkcd()
+
+
+
+def showNumSol(plt, Z1, Z2, fy_an, title):
+	plt.subplot(211)
+	plt.title(title)
+	vt = Z1[:,0]
+	vy1 = Z1[:,1]
+	vy2 = Z2[:,1]
+	plt.plot(vt, vy1, 'bo', markevery=5, color='r', label="y1")
+	plt.plot(vt, vy2, 'k', markevery=5, color='b', label="y2")
+	plt.ylabel("y(t)")
+	plt.legend();
+	plt.grid(True)
+
+	plt.subplot(212)
+	vt = Z1[:,0]
+	vy = Z1[:,1]
+	plt.plot(vt, vy, 'bo', markevery=5, color='r', label="Numerical")
+	plt.plot(vt, fy_an(vt), 'k', markevery=5, color='b', label="Analytical")
+	plt.ylabel("y(t)")
+	plt.legend();
+	plt.grid(True)
+
+	plt.show()
+
+showNumSol(plt, Z1, Y2, y_ex1, title1)
