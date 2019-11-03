5e88f388dfdfa20d99063b743fb59f958e5c09a7
Denis <std.approach@gmail.com>
2019-11-03 17:46:09 +0100

    [2dVectorSpace] UPdate to show 4 charts as table

--- 2dVectorSpace/t1.py
+++ 2dVectorSpace/t1.py
@@ -19,7 +19,7 @@ def getMaxAbs(U):
     xmax=U[np.unravel_index(abs(U).argmax(), U.shape)]
     return xmax
 
-def crtChartQuiver(U, V, ax):
+def crtChartQuiver(U, V, ax, title=''):
     n = U.shape[0]
     X = np.zeros(n)
     Y = np.zeros(n)
@@ -28,6 +28,7 @@ def crtChartQuiver(U, V, ax):
     colors[math.floor(n/4)]='g'
     ax.quiver(X, Y, U, V, units='xy' ,scale=1, color=colors)
     #ax.quiver([0], [0], [0.5], [0.2], units='xy' ,scale=1, color='y', linestyle='-.')
+    ax.set_title(title)
     setupScale(U, V, ax)
     plt.grid()
     ax.set_aspect('equal')
@@ -50,31 +51,52 @@ def show2ChartAsRow1(x1, y1, x2, y2):
     fig.tight_layout()
     plt.show()
 
-#A = np.array([[ -1, -2 ], [ -1, 4 ]])#negative matrix
-A = np.array([[ 3, -2 ], [ -1, 4 ]])
+def show4Chart(x1, y1, x2, y2, x3, y3, x4, y4, title1, title2, title3, title4):
+    fig, axes = plt.subplots(nrows=2, ncols=2)#, figsize=(5, 3))
+    crtChartQuiver(x1, y1, axes[0,0], title1)
+    crtChartQuiver(x2, y2, axes[0,1], title2)
+    crtChartQuiver(x3, y3, axes[1,0], title3)
+    crtChartQuiver(x4, y4, axes[1,1], title4)
+    fig.tight_layout()
+    plt.show()
 
-N = 10
-#
-arr = np.empty([0,6])
-v_a1 = newVec()
-v_a2 = newVec()
-v_b1 = newVec()
-v_b2 = newVec()
-for i in range(N):
-    phi = 2*math.pi*i/N
-    a1 = math.cos(phi)
-    a2 = math.sin(phi)
-    v_a1 = np.vstack((v_a1, np.array([a1])))
-    v_a2 = np.vstack((v_a2, np.array([a2])))
-    vec_a = np.array([ a1, a2 ])
-    vec_b = matrMultVect(A, vec_a)
-    b1 = vec_b[0]
-    b2 = vec_b[1]
-    arr = np.vstack((arr, np.array([i, phi, a1, a2, b1, b2])))
-    v_b1 = np.vstack((v_b1, np.array([b1])))
-    v_b2 = np.vstack((v_b2, np.array([b2])))
-print ("arr=", arr)
+A1 = np.array([[ -1, -2 ], [ -1, 4 ]])#negative matrix
+A2 = np.array([[ 3, -2 ], [ -1, 4 ]])
+N = 32
 
-#printAsRose(v_a1, v_a2)
-#printAsRose(v_b1, v_b2)
-show2ChartAsRow1(v_a1, v_a2, v_b1, v_b2)
+def crtChartTRansferPlaneByMatrix(A1, A2, n):
+    arr = np.empty([0,6])
+    v_a1 = newVec()
+    v_a2 = newVec()
+    v_b1 = newVec()
+    v_b2 = newVec()
+    v_c1 = newVec()
+    v_c2 = newVec()
+    for i in range(n):
+        phi = 2*math.pi*i/n
+        a1 = math.cos(phi)
+        a2 = math.sin(phi)
+        v_a1 = np.vstack((v_a1, np.array([a1])))
+        v_a2 = np.vstack((v_a2, np.array([a2])))
+        vec_a = np.array([ a1, a2 ])
+        vec_b = matrMultVect(A1, vec_a)
+        vec_c = matrMultVect(A2, vec_a)
+        b1 = vec_b[0]
+        b2 = vec_b[1]
+        c1 = vec_c[0]
+        c2 = vec_c[1]
+        arr = np.vstack((arr, np.array([i, phi, a1, a2, b1, b2])))
+        v_b1 = np.vstack((v_b1, np.array([b1])))
+        v_b2 = np.vstack((v_b2, np.array([b2])))
+        v_c1 = np.vstack((v_c1, np.array([c1])))
+        v_c2 = np.vstack((v_c2, np.array([c2])))
+    #print ("arr=", arr)
+
+    #printAsRose(v_a1, v_a2)
+    #printAsRose(v_b1, v_b2)
+    #show2ChartAsRow1(v_a1, v_a2, v_b1, v_b2)
+    s1 = np.array2string(A1, precision=2, separator=',', suppress_small=True)
+    s2 = np.array2string(A2, precision=2, separator=',', suppress_small=True)
+    show4Chart(v_a1, v_a2, v_b1, v_b2, v_a1, v_a2, v_c1, v_c2, 'negative matrix*R2-->', s1, 'positive matrix*R2-->', s2)
+
+crtChartTRansferPlaneByMatrix(A1, A2, N)
